fbaa5c77e6fcd2992e9bca6d81e8363af123ccb1
Liiigia <Ligia.rosa@aluno.senac.rio>
2025-12-16 21:27:26 +0300

    Atividade01

--- aula10/atividade01.py
+++ aula10/atividade01.py
@@ -1,2 +1,43 @@
 # Atividade 01
 # VsCode -  Github
+# receba 3 pares de números e faça a soma de cada par, mostrando o resultado.  
+# Requisitos: funções, laços de repetição e Tratamento de Exceções
+ 
+# 1 - Solicitar dois valores ao usuário. 
+# 2 - Somar os valores. 
+# 3 - Repetir a operação 3 vezes. 
+# 4 - Tratar possíveis erros de entrada com try/except... 
+# 5 - Exibir o resultado da soma quando não houver erro. 
+
+
+
+def soma(v1 , v2):
+    t = v1 + v2
+    return t
+
+
+for i in range(1, 4):
+    try:
+        valor1 = float(input('Digite um valor1: '))
+        valor2 = float(input('Digite um valor1: '))
+        total = soma(valor1, valor2)
+    print(total)
+
+    except ValueError:
+        print('\Erro, digite apenas números')
+    else:
+        print('\Tudo ok')
+    finally:
+        print('Processo finalizado')
+
+    
+    
+
+
+    
+
+
+        
+
+
+
--- aula10/tempCodeRunnerFile.py
+++ aula10/tempCodeRunnerFile.py
@@ -1,19 +1,4 @@
-
-for num in range(1, 4):
-    try:
-        venda = float(input('Informe o valor da venda: '))
-        meta = float(input('Informe a meta: '))
-        resultado = calcula_atingimento(venda, meta)
-        # print(f'Resultado: {resultado}')      # retiro o print daqui e jogo no else, tbm é uma opção
-
-    except ValueError:
-        print('\nErro!!! Digite apenas números')
-    except ZeroDivisionError:
-        print('\nErro!!! A meta não pode ser zero')
-        exit()
-    except KeyboardInterrupt:
-        print('\nOperação finalizada pelo usuário')
-    else:                                                       # else é quando não tiver nenhum erro, não entrou nos erros sinalizado anteriormente.
-        print(f'Resultado: {resultado}%')
-    finally:
-        print('Operação Finalizada')
+valor1 = float(input('Digite um valor1: '))
+    valor2 = float(input('Digite um valor1: '))
+    total = soma(valor1, valor2)
+    print(total)
